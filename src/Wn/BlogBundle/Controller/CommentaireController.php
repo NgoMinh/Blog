1205d3dbed0f2238ab4bed7339f53e9b1501bda2
NgoMinh <NgoWilliam94@gmail.com>
2014-01-25 00:36:14 +0100

    call viewOnElement for the rendering of the comments part

--- src/Wn/BlogBundle/Controller/CommentaireController.php
+++ src/Wn/BlogBundle/Controller/CommentaireController.php
@@ -4,8 +4,43 @@ namespace Wn\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Wn\BlogBundle\Entity\Element;
+use Wn\BlogBundle\Entity\Commentaire;
+
+use Wn\BlogBundle\Form\CommentaireType;
+
 class CommentaireController extends Controller
 {
+	public function viewOnElementAction(Element $element){
+		$em = $this->getDoctrine()
+		           ->getManager();
+
+		$commentaire  = new Commentaire;
+		$commentaire->setAuteur($this->getUser());
+		$commentaire->setElement($element);
+		$commentaires = $em->getRepository('WnBlogBundle:Commentaire')
+		                   ->findByIdElementOrderByDate($element->getId());
+		$form         = $this->createForm(new CommentaireType, $commentaire);
+		$request      = $this->getRequest();
+		if($request->getMethod() == "POST"){
+			$form->bind($request);
+			if($form->isValid()){
+				$em->persist($commentaire);
+				$em->flush();
+
+				return $this->redirect($this->generateUrl('wn_blog_article_voir', array(
+					'id' => $element->getId()
+				)));
+			}
+		}
+
+		return $this->render('WnBlogBundle:Commentaire:view.html.twig',array(
+			'form'         => $form->createView(),
+			'commentaires' => $commentaires,
+			'commentaire'  => $commentaire
+		));
+	}
+
 	public function censurerAction(Commentaire $commentaire){
 		return $this->render('WnBackendBundle:Commentaire:censurer.html.twig');
 	}
